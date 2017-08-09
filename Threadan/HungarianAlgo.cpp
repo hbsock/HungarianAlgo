75e6c565f05ac043e686558b2bc8e2feae04d949
Hanbin Sock <hanbin.sock@magna.com>
2017-08-09 16:38:08 -0400

    Now works!

--- Threadan/HungarianAlgo.cpp
+++ Threadan/HungarianAlgo.cpp
@@ -33,6 +33,98 @@ namespace
 			throw std::exception("No valid assignment.");
 	} //End optimization
 
+
+	  // A DFS based recursive function that returns true if a
+	  // matching for vertex u is possible
+	bool bpm(const std::vector<std::vector<bool>> &bpGraph, 
+		int u, 
+		std::vector<bool> &seen, 
+		std::vector<size_t> &matchR)
+	{
+		// Try every job one by one
+		for (int v = 0; v < bpGraph.front().size(); v++)
+		{
+			// If applicant u is interested in job v and v is
+			// not visited
+			if (bpGraph[u][v] && !seen[v])
+			{
+				seen[v] = true; // Mark v as visited
+
+								// If job 'v' is not assigned to an applicant OR
+								// previously assigned applicant for job v (which is matchR[v]) 
+								// has an alternate job available. 
+								// Since v is marked as visited in the above line, matchR[v] 
+								// in the following recursive call will not get job 'v' again
+				if (matchR[v] == INVALID_ASSIGNMENT || bpm(bpGraph, matchR[v], seen, matchR))
+				{
+					matchR[v] = u;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	// Returns maximum number of matching from M to N
+	std::vector<size_t> maxBPM(const std::vector<std::vector<bool>> &bpGraph)
+	{
+		// An array to keep track of the applicants assigned to
+		// jobs. The value of matchR[i] is the applicant number
+		// assigned to job i, the value -1 indicates nobody is
+		// assigned.
+		std::vector<size_t> matchR(bpGraph.size(), INVALID_ASSIGNMENT);
+
+		int result = 0; // Count of jobs assigned to applicants
+		for (int u = 0; u < bpGraph.size(); u++)
+		{
+			// Mark all jobs as not seen for next applicant.
+			std::vector<bool> seen(bpGraph.at(u).size(), false);
+
+			// Find if the applicant 'u' can get a job
+			if (bpm(bpGraph, u, seen, matchR))
+				result++;
+		}
+		return matchR;
+	}
+
+	void markCostMatrix(size_t row, 
+		size_t maxRow, 
+		size_t maxCol,
+		const std::vector<std::vector<float>> &tempCostMatrix,
+		std::vector<bool> &ColMarked,
+		std::vector<bool> &RowMarked,
+		const std::vector<size_t> &coveredResults)
+	{
+		if (RowMarked[row] == false)
+		{
+			return;
+		}
+
+		std::vector<bool> ColMarkedInThisRow(maxCol, false);
+		for (size_t col = 0; col < maxCol; ++col)
+		{
+			if (tempCostMatrix[row][col] == 0)
+			{
+				ColMarked[col] = true;
+				ColMarkedInThisRow[col] = true;
+			}
+		}
+
+		for (size_t col = 0; col < maxCol; ++col)
+		{
+			if (ColMarkedInThisRow[col])
+			{
+				for (size_t r = 0; r < maxRow; ++r)
+				{
+					if (coveredResults[r] == col && !RowMarked[r])
+					{
+						RowMarked[r] = true;
+						markCostMatrix(r, maxRow, maxCol, tempCostMatrix, ColMarked, RowMarked, coveredResults);
+					}
+				}
+			}
+		}
+	}
 }
 
 std::vector<size_t> AssignmentProblemSolver::DoAlgo(const std::vector<std::vector<float>>& CostMatrix)
@@ -78,37 +170,25 @@ std::vector<size_t> AssignmentProblemSolver::DoAlgo(const std::vector<std::vecto
 		std::vector<bool> RowMarked(maxRow, false);
 		std::vector<bool> ColMarked(maxCol, false);
 		//auto coveredResults = optimization(tempCostMatrix);
-		auto coveredResults = getMaxFlow(tempCostMatrix);
+
+		std::vector<std::vector<bool>> BipartiteGraph(maxRow); 
+		for (size_t row = 0; row < maxRow; ++row)
+		{
+			for (size_t col = 0; col < maxCol; ++col)
+			{
+				BipartiteGraph.at(row).resize(maxCol);
+				BipartiteGraph[row][col] = (tempCostMatrix[row][col] == 0);
+			}
+		}
+		
+		auto coveredResults = maxBPM(BipartiteGraph);
 
 		for (size_t row = 0; row < coveredResults.size(); ++row)
 		{
-			std::vector<bool> ColMarkedInThisRow(maxCol, false);
 			if (coveredResults[row] == INVALID_ASSIGNMENT)
 			{
 				RowMarked[row] = true;
-				
-				for (size_t col = 0; col < maxCol; ++col)
-				{
-					if (tempCostMatrix[row][col] == 0)
-					{
-						ColMarked[col] = true;
-						ColMarkedInThisRow[col] = true;
-					}
-				}
-
-				for (size_t col = 0; col < maxCol; ++col)
-				{
-					if (ColMarkedInThisRow[col])
-					{
-						for (size_t r = 0; r < maxRow; ++r)
-						{
-							if (tempCostMatrix[r][col] == 0)
-							{
-								RowMarked[r] = true;
-							}
-						}
-					}
-				}
+				markCostMatrix(row, maxRow, maxCol, tempCostMatrix, ColMarked, RowMarked, coveredResults);
 			}
 		}
 		
